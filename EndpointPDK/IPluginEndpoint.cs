95271a8544c645b46239825083c517d88fed8dc1
Mikolaj-Bochenek <mikolajbochenek.dev@gmail.com>
2023-09-27 10:06:57 +0200

    add plugin

---
+++ EndpointPDK/IPluginEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace EndpointPDK;
+
+public interface IPluginEndpoint
+{
+    Task Execute(HttpContext ctx);
+}
+
+public class PathAttribute : Attribute
+{
+    public string Method { get; }
+    public string Path { get; }
+    
+    public PathAttribute(string method, string path)
+    {
+        Method = method;
+        Path = path;    
+    }
+}
---
+++ TestEndpoint/TestEndpoint.cs
@@ -0,0 +1,13 @@
+﻿using EndpointPDK;
+using Microsoft.AspNetCore.Http;
+
+namespace TestEndpoint;
+
+[Path("get", "/plug/test")]
+public class TestEndpoint : IPluginEndpoint
+{
+    public async Task Execute(HttpContext ctx)
+    {
+        await ctx.Response.WriteAsync("test 2");
+    }
+}
